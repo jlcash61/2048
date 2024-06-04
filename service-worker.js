b7c39addf03d62f098c124ed0e3bd44aea0bc42d
Jeff Cash <tiborg.iot@gmail.com>
2024-06-04 09:52:39 -0400

    AppV6.1

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = 'phaser-2048-cache-v6';
+const CACHE_NAME = 'phaser-2048-cache-v6.1';
 const urlsToCache = [
   '/2048/',
   '/2048/index.html',
   '/2048/style.css',
   '/2048/app.js',
   '/2048/manifest.json',
-  '/2048/icon.png',
-  '/2048/icon-large.png',
+  '/2048/images/2048icon192.png',
+  '/2048/images/2048icon512.png',
   'https://cdnjs.cloudflare.com/ajax/libs/phaser/3.55.2/phaser.min.js'
 ];
 
